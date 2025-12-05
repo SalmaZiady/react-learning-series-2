75a24363eabd8802125de651f88d514a1824e498
SalmaZiady <salma.elziady@yahoo.com>
2025-12-05 16:21:22 +0200

    feat: add Fruits component with array rendering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Hello from "./components/Hello";
+import Fruits  from "./components/Fruits";
 
 function App() {
-  const person= {
-    name: "Rob",
-    message: "Hi there!",
-    emoji:"👋",
-    seatNumbers: [1, 4, 7],
-  };
   return (
     <div className="App">
-      <Hello person={person}
-      />
+      <Fruits />
     </div>
   );
 }
