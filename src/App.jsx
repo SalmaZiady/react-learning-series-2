2df45231444df76b6a1d2906c2ba93a7be980a55
SalmaZiady <salma.elziady@yahoo.com>
2025-12-06 21:00:03 +0200

    feat: implement conditional rendering logic to toggle Code and Welcome components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import ConditionalComponent from "./components/ConditionalComponent";
 import Fruits  from "./components/Fruits";
 
 function App() {
   return (
     <div className="App">
-      <Fruits />
+     <ConditionalComponent/>
     </div>
   );
 }
