2a87289e7f374e19850de449e4fbc277279ad60c
SalmaZiady <salma.elziady@yahoo.com>
2025-12-30 23:14:53 +0200

    feat(form): implement controlled input fields with state handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import ConditionalComponent from "./components/ConditionalComponent";
 import Counter from "./components/Counter";
+import Form from "./components/Form";
 import  Message  from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-     <Counter/>
+     <Form/>
     </div>
   );
 }
