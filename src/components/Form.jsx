17903e9051f54a67ca29bbea7b708bd22123a273
SalmaZiady <salma.elziady@yahoo.com>
2025-12-30 23:30:27 +0200

    feat(form): handle multiple input fields with controlled state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setName] = useState("");
-  
+  const [name, setName] = useState({ firstName: "", lastName: "" });
 
   return (
     <div>
+      {name.firstName} - {name.lastName}
       <form>
-        <input onChange={(e) =>  setName(e.target.value)} type="text" value={name} />
+        <input
+          onChange={(e) => setName({ ...name, firstName: e.target.value })}
+          type="text"
+          value={name.firstName}
+        />
+        <input
+          type="text"
+          onChange={(e) => setName({ ...name, lastName: e.target.value })}
+          value={name.lastName}
+        />
       </form>
     </div>
   );
